d0d35914381f551d1267f107e5ee8461dbbf07ce
Navya kanneti <121533303+22A91A4459@users.noreply.github.com>
2023-08-24 23:22:19 +0530

    import java.util.Scanner; public class sample{     public static void main(String args[]){         Scanner sc=new Scanner(System.in);         int n,c;         n=sc.nextInt();         if(n%4==0)         {             System.out.println(n/4);         }         else         {             System.out.println((n/4)+1);         }     } }

---
+++ Cars.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class sample{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n,c;
+        n=sc.nextInt();
+        if(n%4==0)
+        {
+            System.out.println(n/4);
+        }
+        else
+        {
+            System.out.println((n/4)+1);
+        }
+    }
+}
