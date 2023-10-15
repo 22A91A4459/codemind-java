abf26a81e235c06da39dac23cdae1e8638507795
Navya kanneti <121533303+22A91A4459@users.noreply.github.com>
2023-10-15 14:51:28 +0530

    import java.util.Scanner; public class Sample{     public static void main(String args[]){         Scanner sc=new Scanner(System.in);         String s=sc.nextLine();         int count=0;         for(int i=0;i<s.length();i++){             char ch=s.charAt(i);             if(ch >= 'a' && ch <= 'z'){                 count++;             }         }         System.out.println(count);     } }

---
+++ Lowercase_Letters_in_a_String.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Sample{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        int count=0;
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            if(ch >= 'a' && ch <= 'z'){
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
