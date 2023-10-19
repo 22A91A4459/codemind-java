c4663faff9b8b5ffca704ff6c0c3248b14e22246
Navya kanneti <121533303+22A91A4459@users.noreply.github.com>
2023-10-19 10:49:14 +0530

    import java.util.Scanner; public class Sample{     public static void main(String args[]){         Scanner sc=new Scanner(System.in);         int n,k;         n=sc.nextInt();         k=sc.nextInt();         int c=0;         int arr [] =new int[n];         for(int i=0;i<n;i++){             arr[i]=sc.nextInt();         }         for(int i=0;i<n;i++){             if(arr[i]%k!=0){                 c++;             }         }         System.out.println(c);     } }

---
+++ Count_elements_not_divisible_by_K.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Sample{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n,k;
+        n=sc.nextInt();
+        k=sc.nextInt();
+        int c=0;
+        int arr [] =new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        for(int i=0;i<n;i++){
+            if(arr[i]%k!=0){
+                c++;
+            }
+        }
+        System.out.println(c);
+    }
+}
