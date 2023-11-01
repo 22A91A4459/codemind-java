1729ede2f6cf87367ec73e8edbd8e8787e9f7efc
Navya kanneti <121533303+22A91A4459@users.noreply.github.com>
2023-11-01 11:27:43 +0530

    import java.util.*; public class Ascsii {     public static void main(String[] args) {     Scanner sc=new Scanner(System.in);     char ch = sc.next().charAt(0); ;     int as_chi = ch;     System. out. println(as_chi);     sc.close();     }
        }

---
+++ Find_ASCII_Value_of_a_Character.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Ascsii {
+    public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    char ch = sc.next().charAt(0); ;
+    int as_chi = ch;
+    System. out. println(as_chi);
+    sc.close();
+    }
+    
+    }
